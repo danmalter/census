a83a52961d96381e3cf948b64fe53ca2e2aa4863
Danny <danmalter@gmail.com>
2017-10-22 10:06:13 -0500

    update

--- R/getCensus.R
+++ R/getCensus.R
@@ -9,7 +9,7 @@
 #' @importFrom utils read.csv
 #' @examples
 #' radius <- 2
-#' cities <- c("Chicago", "New York")
+#' cities <- c("Los Angelas", "Dallas")
 #' df <- getCensus(cities, radius)
 #' head(df)
 #' @export
